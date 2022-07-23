41b73ca28bf570fb990e790819b6bbb7ff21ebcd
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-23 07:09:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function displayCountryInfo(countryByAlpha3Code) {
   document.getElementById("capital").innerHTML = countryData.capital;
   document.getElementById("dialing-code").innerHTML = `+${countryData.callingCodes[0]}`;
   document.getElementById("population").innerHTML = countryData.population.toLocaleString("en-US");
-  document.getElementById("currencies").innerHTML = countryData.currencies.filter(c => c.name).map(c => `{c.name} (${c.code})`).join(", ");
+  document.getElementById("currencies").innerHTML = countryData.currencies.filter(c => c.name).map(c => `{c.name} ({c.code})`).join(", ");
   document.getElementById("region").innerHTML = countryData.region;
   document.getElementById("subregion").innerHTML = countryData.subregion;
 }
