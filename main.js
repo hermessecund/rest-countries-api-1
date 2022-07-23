19943f2e743c177b667a638e0de53246e8501420
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-23 07:14:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -53,10 +53,10 @@ function initialize(countriesData) {
 
 function displayCountryInfo(countryByAlpha3Code) {
   const countryData = countries.find(country => country.alpha3Code === countryByAlpha3Code);
-  document.querySelector("flag-container img").src = countryData.flag;
-  document.querySelector("flag-container img").alt = `Flag of {countryData.name}`;  
+  document.querySelector("#flag-container img").src = countryData.flag;
+  document.querySelector("#flag-container img").alt = `Flag of {countryData.name}`;  
   document.getElementById("capital").innerHTML = countryData.capital;
-  document.getElementById("dialing-code").innerHTML = `+${countryData.callingCodes[0]}`;
+  document.getElementById("dialing-code").innerHTML = `+{countryData.callingCodes[0]}`;
   document.getElementById("population").innerHTML = countryData.population.toLocaleString("en-US");
   document.getElementById("currencies").innerHTML = countryData.currencies.filter(c => c.name).map(c => `{c.name} ({c.code})`).join(", ");
   document.getElementById("region").innerHTML = countryData.region;
